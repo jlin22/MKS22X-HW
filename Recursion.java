a92e773976d1e36d2e9b3f7e4baf4c43d4899d44
John Lin <jlin22@stuy.edu>
2017-02-06 22:42:57 -0500

    finished with recursion+test cases

---
+++ Recursion.java
@@ -0,0 +1,22 @@
+public class Recursion{
+    public static String name(){
+	return "Lin,John";
+    }
+    public static double sqrt(double n){
+	if (n < 0){
+	    throw new IllegalArgumentException();
+	}
+	return helper(n, n);
+    }
+    private static double helper(double n, double guess){
+	if ((guess * guess - n) / n <= 0.000000001) {
+	    return guess;
+	}
+	return helper(n, (n / guess + guess) / 2);
+    }
+    /* public static void main(String[]a){
+	System.out.println(sqrt(100));
+	System.out.println(sqrt(5));
+	System.out.println(sqrt(16.0 / 9));
+	}*/
+}
