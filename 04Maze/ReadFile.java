24ce36cf09a60568367c137412eea41a832c0c02
John Lin <jlin22@stuy.edu>
2017-03-05 18:04:24 -0500

    can read files

--- 04Maze/ReadFile.java
+++ 04Maze/ReadFile.java
@@ -2,23 +2,43 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class ReadFile {
-    public String read(String file){
-	String x = "File not Found";
+    private String filetext;
+    private char[][] maze;
+    public ReadFile(String file){
+	filetext = "";	
 	try{
-	    x = "";
-	    File text = new File(file);
+	    File text = new File("data1.dat");
 	    Scanner inf = new Scanner(text);
-	    int lineNumber = 1;
+	    int lineNumber = 0;
 	    while(inf.hasNextLine()){
 		String line = inf.nextLine();
-		x += inf.nextLine();
+	        filetext += line;
+		lineNumber += 1;
+	    }
+	    maze = new char[lineNumber][filetext.length() / lineNumber];
+	    for (int c = 0; c < lineNumber; c++){
+		for (int i = 0; i < filetext.length() / lineNumber; i++){
+		     maze[c][i] = filetext.charAt(c * filetext.length() / lineNumber + i);
+		 }
 	    }
 	}catch (FileNotFoundException e){
-	    return "File not Found";
 	}
-	return "File not Found";
     }
-	        
+    public char[][] getMaze(){
+	return maze;
+    }
+    public String toString(){
+	String a = "";
+	for (int c = 0; c < maze.length; c++){
+	    for (int r = 0; r < maze[c].length; r++){
+	        a += maze[c][r];
+		if (r == maze[c].length - 1){
+		    a += "\n";
+		}
+	    }
+	}
+	return a;
+    }
   public static void main(String args[]) throws FileNotFoundException {
         //instead of a try/catch, you can throw the FileNotFoundException.
       /* File text = new File("data1.dat");// can be a path"/full/path/to/file.txt" 
@@ -28,7 +48,9 @@ public class ReadFile {
             String line = inf.nextLine();
             System.out.println(line);
       */
-      ReadFile x = new ReadFile();
-      System.out.println(x.read("data1.dat"));
-        }       
-    }   
+      ReadFile x = new ReadFile("data1.dat");
+      System.out.println(x.toString());
+      System.out.println(x.getMaze());
+      
+  }       
+}   
