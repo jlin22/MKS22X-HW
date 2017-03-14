1df2817490926c8f5eaf00decb707ce523055b03
John Lin <jlin22@stuy.edu>
2017-03-14 12:40:21 -0400

    finished partition

---
+++ 07/Partition.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+public class Partition{
+    public int part(int[]data, int start, int end){
+	int x = (int)(Math.random()*(end - start +1)) + start;
+	int pivot = data[x];
+	int s = start;
+	int e = end;
+	int[] a = new int[data.length];
+	for (int i = 0; i >= start && i <= end; i++){
+	    if (i != x){
+		if (data[i] < pivot){
+		    a[s] = data[i];
+		    s++;
+		}
+		if (data[i] > pivot){
+		    a[e] = data[i];
+		    e--;
+		}
+	    }
+	}
+	a[s] = pivot;
+	data = a;
+	for (int i = 0; i < a.length; i++){
+	    System.out.println(a[i]);
+	}
+	return s;
+    }
+    public static void main(String[]arg){
+	Partition p = new Partition();
+	int[] a = {0,2,1,3};
+	System.out.println(p.part(a, 0, 3));
+
+
+    }
+}
