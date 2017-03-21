3fc661c90631f57ec99636cbda03112c43b4e2bd
John <jlin22@stuy.edu>
2017-03-21 14:33:36 -0400

    thought I pushed earlier, but didn't get pushed

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -9,14 +9,17 @@ public class Recursion{
 	return helper(n, n);
     }
     private static double helper(double n, double guess){
+	if (n == 0){
+	    return 0;
+	}
 	if ((guess * guess - n) / n <= 0.000000001) {
 	    return guess;
 	}
 	return helper(n, (n / guess + guess) / 2);
     }
-    /* public static void main(String[]a){
-	System.out.println(sqrt(100));
-	System.out.println(sqrt(5));
+    public static void main(String[]a){
+	System.out.println(sqrt(0));
+	System.out.println(sqrt(-5));
 	System.out.println(sqrt(16.0 / 9));
-	}*/
+    }
 }
