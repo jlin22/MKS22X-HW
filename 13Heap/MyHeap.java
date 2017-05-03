f67a53b8ada45466a3038b4e2001b3c11a77000a
John Lin <jlin22@stuy.edu>
2017-05-03 14:45:27 -0400

    started heap

---
+++ 13Heap/MyHeap.java
@@ -0,0 +1,18 @@
+import java.util.io.*;
+public class MyHeap{
+    private ArrayList<String> ary;
+    private int size;
+    public MyHeap(){
+	ary = new ArrayList<String>();
+	ary.add("");
+	size = 0;
+    }
+    public MyHeap(boolean h){
+	if (h){
+	}
+	else{
+	}
+    }
+    
+    
+}
