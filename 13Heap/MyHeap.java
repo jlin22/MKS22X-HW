aecee0bd53a379bbf42ae6ba3873398e5dea5aa6
John Lin <jlin22@stuy.edu>
2017-05-04 14:38:16 -0400

    started heap

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -2,6 +2,7 @@ import java.util.io.*;
 public class MyHeap{
     private ArrayList<String> ary;
     private int size;
+    private boolean min;
     public MyHeap(){
 	ary = new ArrayList<String>();
 	ary.add("");
@@ -9,9 +10,32 @@ public class MyHeap{
     }
     public MyHeap(boolean h){
 	if (h){
+	    ary = new ArrayList<String>();
+	    ary.add("");
+	    size = 0;
+	    min = false;
 	}
 	else{
+	    ary = new ArrayList<String>();
+	    ary.add("");
+	    size = 0;
+	    min = true;;
 	}
+    }
+    public void add(String s){
+	ary.add(s);
+	size++;
+    }
+    public String remove(){
+	size--;
+	return ary.remove(size);
+    }
+    public String peek(){
+	return ary.get(size);
+    }
+    private void pushUp(){	
+    }
+    private void pushDown(){
     }
     
     
