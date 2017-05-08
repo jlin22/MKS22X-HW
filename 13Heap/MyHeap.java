d4ee5589cd710e1c6fbd687e089c38dd0e38dbfc
John Lin <jlin22@stuy.edu>
2017-05-08 14:45:05 -0400

    almost done with Heap

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -25,17 +25,92 @@ public class MyHeap{
     public void add(String s){
 	ary.add(s);
 	size++;
+	int i = size;
+	if (min){
+	    while (ary[i]<ary[i/2] && i != 1){
+		pushUp(i);
+		i = i/2;
+	    }
+	}
+	else{
+	    while(ary[i] > ary[i/2] && i!= 1){
+		pushup(i);
+		i = i/2;
+	    }
+	}		
     }
     public String remove(){
+	String x = ary[1];
+	String y = ary[size];
+	ary[1] = ary[size];
 	size--;
-	return ary.remove(size);
+	i = 1;
+	while (i <= size/2){
+	    if (min){
+		if (ary[i] > ary[2i] && ary[i] > ary[2i+1]){
+		    pushDown(i);
+		    if (ary[2i] = y){
+			i = 2i;
+		    }
+		    else{
+			i = 2i+1;
+		    }
+		}
+		else{
+		    return x;
+		}
+	    }
+	    else{
+		if (ary[i] <ary[2i] && ary[i] < ary[2i+1]){
+		    pushDown(i);
+		    if (ary[2i] = y){
+			i = 2i;
+		    }
+		    else{
+			i = 2i+1;
+		    }
+		}
+		else{
+		    return x;
+		}
+	    }
+	}	        			    
+	return x;
     }
     public String peek(){
 	return ary.get(size);
     }
-    private void pushUp(){	
+    private void pushUp(int x){
+	int temp = ary[x/2];
+	ary[x/2] = ary[x];
+	ary[x] = temp;
     }
-    private void pushDown(){
+    private void pushDown(int x){
+	int temp = ary[x];
+	int a = ary[2x];
+	int b = ary[2x+1];
+	if (min){
+	    //5,3,4
+	    if (a <= b){
+		ary[x] = a;
+		ary[2x] = temp;
+	    }
+	    else{
+		ary[x] = b;
+		ary[2x+1] = temp;
+	    }
+	}	
+	else{
+	    //3,7,5
+	    if (a >= b){
+		ary[x] = a;
+		ary[2x] = temp;
+	    }
+	    else{
+		ary[x] = b;
+		ary[2x+1] = temp;
+	    }
+	}
     }
     
     
