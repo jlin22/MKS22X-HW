e5a30aef1c97e699b1519f4437636586ff8498a3
John Lin <jlin22@stuy.edu>
2017-05-15 14:46:08 -0400

    started location and frontier

---
+++ 15Maze/Frontier.java
@@ -0,0 +1,4 @@
+public interface Frontier{
+    public void add(Location x);
+    public Location next();
+}
---
+++ 15Maze/Location.java
@@ -0,0 +1,45 @@
+public class Location implements Comparable<Location>{
+    private int row;
+    private int col;
+    private Location previous;
+    private int dts;
+    private int dtg;
+    private boolean aStar;
+    public Location(int r, int c, Location prev, int DS, int DG){
+	row = r;
+	col = c;
+	previous = prev;
+	dts = DS;
+	dtg = DG;
+	aStar = true;
+    }
+    public Location(int r, int c, Location prev, int DS, int DG, boolean a){
+	row = r;
+	col = c;
+	previous = prev;
+	dts = DS;
+	dtg = DG;
+	aStar = true;
+    }
+    public int getDTS(){
+	return dts;
+    }
+    public int getDTG(){
+	return dtg;
+    } 
+    public Location getPrev(){
+	return previous;
+    }
+    public int compareTo(Location other){
+	if(aStar){
+	    return dts+dtg-other.getDTS()-other.getDTG();
+	}
+	else{
+	    return dtg-other.getDTG();
+	}
+	
+    }
+    public static void main(String[]a){
+	Location x = new Location(0,0,null,0,5,false);
+    }
+}
