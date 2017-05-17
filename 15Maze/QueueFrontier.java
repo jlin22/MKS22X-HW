df996b28d95cea6d7aa3e4fee17f6e5730dd7883
John Lin <jlin22@stuy.edu>
2017-05-17 14:45:38 -0400

    done with frontier queue and stack

--- 15Maze/QueueFrontier.java
+++ 15Maze/QueueFrontier.java
@@ -1,26 +1,27 @@
-import java.util.Stack;
-public class StackFrontier implements Frontier{
-    private Stack<Location> front;
-    public StackFrontier(){
-	front = new Stack<Location>();
+import java.util.LinkedList;
+public class QueueFrontier implements Frontier{
+    private LinkedList<Location> front;
+    public QueueFrontier(){
+	front = new LinkedList<Location>();
     }
     public void add(Location x){
-	front.push(x);
+	front.add(x);
+	
     }
     public Location next(){
-	return front.pop();
+	return front.removeLast();
     }
     public int size(){
 	return front.size();
     }
-    /* public static void main(String[]a){
+    /*public static void main(String[]a){
 	Location x = new Location(0,0,null,0,5,false);
 	Location y = new Location(1,1,x,4,0,false);
-	StackFrontier z = new StackFrontier();
+	QueueFrontier z = new QueueFrontier();
 	z.add(x);
 	z.add(y);
 	System.out.println(z.size());
 	System.out.println(z.next());
 	System.out.println(z.next());
-	}*/
+    }*/
 }
